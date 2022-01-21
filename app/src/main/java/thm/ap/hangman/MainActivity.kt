f56d3b359abdb550595dc31d430d18af800f8796
mohammad <mohammad.alayoub@mni.thm.de>
2022-01-21 02:49:20 +0100

    Add Ticket[#1]

--- app/src/main/java/thm/ap/hangman/MainActivity.kt
+++ app/src/main/java/thm/ap/hangman/MainActivity.kt
@@ -1,26 +1,27 @@
 package thm.ap.hangman
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import thm.ap.hangman.models.Category
-import thm.ap.hangman.models.Competition
-import thm.ap.hangman.models.Player
-import thm.ap.hangman.models.Statistic
 import thm.ap.hangman.persistence.*
 
 class MainActivity : AppCompatActivity() {
-    private val auth = Auth()
+    private val auth = Auth(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (auth.authenticate()) {
             Toast.makeText(this, "You are logged in", Toast.LENGTH_SHORT).show()
         } else {
+            auth.signUp()
             // TODO: sign in OR sign up
-            throw NotImplementedError();
+            //throw NotImplementedError();
         }
+
+        val playerDAO = PlayerDAO()
+
+        //playerDAO.addPlayer(Player.new(Firebase.auth.currentUser!!.uid, "Moh", Statistic.empty(), Status.OFFLINE))
+
         setContentView(R.layout.activity_main)
     }
 }
