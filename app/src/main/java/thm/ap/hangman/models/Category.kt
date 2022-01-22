de052702a36e1c305243dec529ef449a7be9cb69
mohammad <mohammad.alayoub@mni.thm.de>
2022-01-22 20:36:25 +0100

    Configure the services to use the Result-Class

--- app/src/main/java/thm/ap/hangman/models/Category.kt
+++ app/src/main/java/thm/ap/hangman/models/Category.kt
@@ -9,4 +9,16 @@ data class Category (@set:DocumentId
     var catName: String? = null
     var words: MutableList<String>? = null
 
+    companion object {
+        fun new(catName: String, words: MutableList<String>): Category {
+            val category = Category()
+            category.catName = catName
+            category.words = words
+            return category
+        }
+
+        fun empty(): Category {
+            return Category()
+        }
+    }
 }
--- app/src/main/java/thm/ap/hangman/persistence/CategoryDAO.kt
+++ app/src/main/java/thm/ap/hangman/persistence/CategoryDAO.kt
@@ -7,41 +7,87 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import thm.ap.hangman.models.Category
-
+import thm.ap.hangman.models.Result
 
 class CategoryDAO {
-    private val categoriesRef: CollectionReference = Firebase.firestore.collection("categories")
-    val categoriesObserver: MutableLiveData<List<Category>> by lazy {
+    private val categoriesRef: CollectionReference = Firebase.firestore.collection(TAG)
+    private val categoriesObserver = MutableLiveData<Result<List<Category>>>()
+
+    fun getCategoriesObserver(): MutableLiveData<Result<List<Category>>> {
         refreshCategories()
-        MutableLiveData<List<Category>>()
+        return categoriesObserver
     }
 
     private fun refreshCategories() {
-        categoriesRef.get().addOnSuccessListener {
-            val cats = mutableListOf<Category>()
-            it.forEach { doc ->
-                val category = doc.toObject<Category>()
-                cats.add(category)
+        categoriesObserver.value = Result.inProgress()
+        categoriesRef.get()
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    val cats = mutableListOf<Category>()
+                    task.result.forEach { doc ->
+                        val category = doc.toObject<Category>()
+                        cats.add(category)
+                    }
+                    cats.removeIf { cat -> cat.id == "baseline" }
+                    categoriesObserver.value = Result.success(cats)
+                } else {
+                    categoriesObserver.value = Result.failure(task.exception!!.message!!)
+                }
             }
-            categoriesObserver.value = cats
-        }
     }
 
-    fun addCategory(category: Category) {
-        categoriesRef.document().set(category).addOnSuccessListener {
-            refreshCategories()
-        }
+    fun addCategory(category: Category): MutableLiveData<Result<Category>> {
+        val observer = MutableLiveData<Result<Category>>()
+
+        observer.value = Result.inProgress()
+        categoriesRef.document().set(category)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    refreshCategories()
+                    observer.value = Result.success(category)
+                } else {
+                    observer.value = Result.failure(task.exception!!.message!!)
+                }
+            }
+
+        return observer
     }
 
-    fun updateCategory(category: Category) {
-        categoriesRef.document(category.id).set(category).addOnSuccessListener {
-            refreshCategories()
-        }
+    fun updateCategory(category: Category): MutableLiveData<Result<Category>> {
+        val observer = MutableLiveData<Result<Category>>()
+
+        observer.value = Result.inProgress()
+        categoriesRef.document(category.id).set(category)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    refreshCategories()
+                    observer.value = Result.success(category)
+                } else {
+                    observer.value = Result.failure(task.exception!!.message!!)
+                }
+            }
+
+        return observer
     }
 
-    fun deleteCategory(category: Category) {
-        categoriesRef.document(category.id).delete().addOnSuccessListener {
-            refreshCategories()
-        }
+    fun deleteCategory(category: Category): MutableLiveData<Result<Category>> {
+        val observer = MutableLiveData<Result<Category>>()
+
+        observer.value = Result.inProgress()
+        categoriesRef.document(category.id).delete()
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    refreshCategories()
+                    observer.value = Result.success(category)
+                } else {
+                    observer.value = Result.failure(task.exception!!.message!!)
+                }
+            }
+
+        return observer
+    }
+
+    companion object {
+        const val TAG = "categories"
     }
 }
