7da936d8aafd8834425afed6a15a66df592eec98
mwak88 <mohamed.walid.abu.khadra@mni.thm.de>
2022-01-25 18:22:29 +0100

    converted to viewbinding

--- app/src/main/java/thm/ap/hangman/fragments/MultiPlayer.kt
+++ app/src/main/java/thm/ap/hangman/fragments/MultiPlayer.kt
@@ -8,6 +8,8 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import thm.ap.hangman.R
+import thm.ap.hangman.databinding.FragmentMainMenuBinding
+import thm.ap.hangman.databinding.FragmentMultiPlayerBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,6 +26,9 @@ class MultiPlayer : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var _binding: FragmentMultiPlayerBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,11 +38,13 @@ class MultiPlayer : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_multi_player, container, false)
+        _binding = FragmentMultiPlayerBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -45,13 +52,12 @@ class MultiPlayer : Fragment() {
 
         val navController = findNavController()
 
-        val buttonEnterRoom: Button = view.findViewById(R.id.button_enter_room)
-        buttonEnterRoom.setOnClickListener {
+        binding.buttonEnterRoom.setOnClickListener {
             val action = MultiPlayerDirections.actionMultiPlayerToChooseWord()
             navController.navigate(action)
         }
-        val buttonCreateRoom: Button = view.findViewById(R.id.button_create_room)
-        buttonCreateRoom.setOnClickListener {
+
+        binding.buttonCreateRoom.setOnClickListener {
             val action = MultiPlayerDirections.actionMultiPlayerToChooseWord()
             navController.navigate(action)
         }
