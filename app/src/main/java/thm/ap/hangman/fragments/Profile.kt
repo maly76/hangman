4ad2f5dc1f6630663fbb3745eba0201a020be53a
mwak88 <mohamed.walid.abu.khadra@mni.thm.de>
2022-01-25 21:44:34 +0100

    adding params back to Profile.kt

--- app/src/main/java/thm/ap/hangman/fragments/Profile.kt
+++ app/src/main/java/thm/ap/hangman/fragments/Profile.kt
@@ -11,6 +11,10 @@ import thm.ap.hangman.databinding.FragmentProfileBinding
 import thm.ap.hangman.models.Statistic
 import thm.ap.hangman.persistence.PlayerDAO
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +22,9 @@ import thm.ap.hangman.persistence.PlayerDAO
  * create an instance of this fragment.
  */
 class Profile : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
@@ -26,6 +33,10 @@ class Profile : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
     override fun onCreateView(
@@ -82,8 +93,8 @@ class Profile : Fragment() {
         fun newInstance(param1: String, param2: String) =
             Profile().apply {
                 arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
             }
     }
