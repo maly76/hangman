64b9a5bd1cf299bcd8fcd74fab7dbbb9c62c3b48
mwak88 <mohamed.walid.abu.khadra@mni.thm.de>
2022-01-25 17:29:34 +0100

    added navigation to fragment_choose_word.xml from fragment_multi_player.xml

--- app/src/main/java/thm/ap/hangman/fragments/ChooseWord.kt
+++ app/src/main/java/thm/ap/hangman/fragments/ChooseWord.kt
@@ -1,12 +1,10 @@
 package thm.ap.hangman.fragments
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import thm.ap.hangman.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -16,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MultiPlayer.newInstance] factory method to
+ * Use the [ChooseWord.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MultiPlayer : Fragment() {
+class ChooseWord : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,24 +35,7 @@ class MultiPlayer : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_multi_player, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val navController = findNavController()
-
-        val buttonEnterRoom: Button = view.findViewById(R.id.button_enter_room)
-        buttonEnterRoom.setOnClickListener {
-            val action = MultiPlayerDirections.actionMultiPlayerToChooseWord()
-            navController.navigate(action)
-        }
-        val buttonCreateRoom: Button = view.findViewById(R.id.button_create_room)
-        buttonCreateRoom.setOnClickListener {
-            val action = MultiPlayerDirections.actionMultiPlayerToChooseWord()
-            navController.navigate(action)
-        }
+        return inflater.inflate(R.layout.fragment_choose_word, container, false)
     }
 
     companion object {
@@ -64,12 +45,12 @@ class MultiPlayer : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MultiPlayer.
+         * @return A new instance of fragment ChooseWord.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MultiPlayer().apply {
+            ChooseWord().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
