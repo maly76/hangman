978b87bd5ca335158cf6cd51f276776eb751ef0f
mohammad <mohammad.alayoub@mni.thm.de>
2022-01-25 13:44:43 +0100

    Document the source code in models

--- app/src/main/java/thm/ap/hangman/models/MultiPlayerGame.kt
+++ app/src/main/java/thm/ap/hangman/models/MultiPlayerGame.kt
@@ -1,6 +1,9 @@
 package thm.ap.hangman.models
 
-
+/**
+ * The infos of a multiplayer game
+ * Is needed in the multiplayergame
+ * */
 class MultiPlayerGame {
     val host: PlayerInfos
     val guest: PlayerInfos
@@ -10,6 +13,13 @@ class MultiPlayerGame {
         guest = PlayerInfos()
     }
 
+    /**
+     * The infos of a player in a multiplayer game
+     * wordToGuess: which word has the player to guess
+     * tries: which number of tries he needed
+     * hiddenWord: which word is still hidden
+     * status: WON, LOSED, PLAYING or OFFLINE
+     * */
     class PlayerInfos {
         var wortToGuess: String? = null
         var tries: Int = 0
--- app/src/main/java/thm/ap/hangman/models/Result.kt
+++ app/src/main/java/thm/ap/hangman/models/Result.kt
@@ -1,13 +1,33 @@
 package thm.ap.hangman.models
 
+/**
+ * The Result class, it is used for events to return a specified status and data
+ * T should be specified and it is the datatype of @property data
+ * @param status to identify the status of the request SUCCESS, IN_PROGRESS or FAILURE
+ * @param data is only set if the status is SUCCESS (the request is successfully)
+ * @param error is only set if the status is FAILURE (the request is failed)
+ * */
 class Result<T>(val status: Status, val data: T? = null, val error: String? = null) {
     enum class Status {
         SUCCESS, IN_PROGRESS, FAILURE
     }
 
     companion object {
+        /**
+         * @param data, the datatype should be the same of the specified T
+         * @return a new object of Result with Status SUCCESS and the data
+         * */
         fun <T> success(data: T?) = Result(Status.SUCCESS, data)
+
+        /**
+         * @param error is the message of the error
+         * @return a new object of Result with the status FAILURE and the specified error
+         * */
         fun <T> failure(error: String) = Result<T>(Status.FAILURE, error = error)
+
+        /**
+         * @return a new object of Result with the status IN_PROGRESS
+         * */
         fun <T> inProgress() = Result<T>(Status.IN_PROGRESS)
     }
 }
