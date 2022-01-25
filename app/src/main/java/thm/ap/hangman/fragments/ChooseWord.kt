2578e72ac069bbb89ac5d0761a736b24be705465
mwak88 <mohamed.walid.abu.khadra@mni.thm.de>
2022-01-25 18:12:12 +0100

    converted playing field to fragment and added navigation

--- app/src/main/java/thm/ap/hangman/fragments/ChooseWord.kt
+++ app/src/main/java/thm/ap/hangman/fragments/ChooseWord.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import thm.ap.hangman.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -36,6 +38,18 @@ class ChooseWord : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_choose_word, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        val buttonOk: Button = view.findViewById(R.id.button_ok)
+        buttonOk.setOnClickListener {
+            val action = ChooseWordDirections.actionChooseWordToPlayingField()
+            navController.navigate(action)
+        }
     }
 
     companion object {
--- app/src/main/java/thm/ap/hangman/fragments/PlayingField.kt
+++ app/src/main/java/thm/ap/hangman/fragments/PlayingField.kt
@@ -1,27 +1,53 @@
-package thm.ap.hangman
+package thm.ap.hangman.fragments
 
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
-import thm.ap.hangman.databinding.PlayingFieldBinding
+import thm.ap.hangman.R
+import thm.ap.hangman.databinding.FragmentPlayingFieldBinding
 import thm.ap.hangman.gamelogic.GameLogic
 
-class PlayingFieldActivity : AppCompatActivity() {
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
-    private lateinit var binding: PlayingFieldBinding
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PlayingField.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class PlayingField : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
-    //    private val viewModel: PlayingFieldViewModel by viewModels()
+    private var _binding: FragmentPlayingFieldBinding? = null
+    private val binding get() = _binding!!
+
     private val gameLogic = GameLogic()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
-        binding = PlayingFieldBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        bindButtons()
-        binding.guessButton.setOnClickListener { guessWord() }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPlayingFieldBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     @SuppressLint("SetTextI18n")
@@ -30,6 +56,12 @@ class PlayingFieldActivity : AppCompatActivity() {
         updateHiddenWord()
         //TODO: write wordToGuess to DB
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 
     @SuppressLint("SetTextI18n")
     private fun onButtonPressed(button: Button) {
@@ -118,4 +150,26 @@ class PlayingFieldActivity : AppCompatActivity() {
         binding.letterOE.setOnClickListener { onButtonPressed(binding.letterOE) }
     }
 
+
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment PlayingField.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            PlayingField().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
